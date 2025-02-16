571b786e0e4a54beb35362c3a732a2d5cfd9b062
Abhishek Negi <47.negi@gmail.com>
2025-02-16 04:27:35 +0000

    basics_js

---
+++ 01_Intro/script.js
@@ -0,0 +1,21 @@
+alert("Hello World")
+
+console.log("Hey i am Console.......")
+console.log("Hey i am second Console.......")
+console.log("Hey i am third Console.......")
+console.log("Hey i am fourth Console.......")
+
+var a = prompt("Enter your Number")
+console.log("Your Number is "+a)
+
+var istrue = confirm("Are you sure want to leave this page and blast your computer")
+if(istrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("You are safe")
+}
+ 
+document.title = "Hey i am good"
+
+document.body.style.background = "red"
